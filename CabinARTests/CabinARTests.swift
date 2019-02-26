2712bdca74de5f56e068c6d31907c63e290504d4
Pascal Rettig <pascal@cykod.com>
2019-02-26 09:34:55 -0500

    Initial Commit

---
+++ CabinARTests/CabinARTests.swift
@@ -0,0 +1,34 @@
+//
+//  CabinARTests.swift
+//  CabinARTests
+//
+//  Created by Pascal Rettig on 2/26/19.
+//  Copyright © 2019 Pascal Rettig. All rights reserved.
+//
+
+import XCTest
+@testable import CabinAR
+
+class CabinARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
